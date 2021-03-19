961e64e10a509d00bdee49b56f525f3822b52733
KANJI <80818995+TioSlown@users.noreply.github.com>
2021-03-19 11:47:19 +0100

    SUMO_1
    19/03/21
    creation fenetre pygame
    ajout du background

---
+++ Sumo/main.py
@@ -0,0 +1,20 @@
+﻿import pygame #import pygame library
+from pygame.locals import *
+
+pygame.init() #initialize pygame
+
+window = pygame.display.set_mode((640, 480)) #creation et definition des paramètres de la fenetre
+
+
+background = pygame.image.load("domo.background.png").convert() #on defini et colle le fond
+window.blit(background, (0, 0))
+
+pygame.display.flip() #rafraichissement de l'image
+
+
+slown = 1
+
+while slown : #boucle qui sert à garder la fenetre ouverte
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            slown=0
