5a812321ff267c5779578faae4b46a99573a83a9
KANJI <80818995+TioSlown@users.noreply.github.com>
2021-03-31 12:01:49 +0200

    Add files via upload

--- Sumo/main.py
+++ Sumo/main.py
@@ -1,20 +1,36 @@
 ﻿import pygame #import pygame library
 from pygame.locals import *
 
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.stamina = 100
+        self.velocity = 5
+        self.image = pygame.image.load('player1.png').convert()
+        self.rect = self.image.get_rect()
+        self.rect.x = 302
+        self.rect.y = 300
+
 pygame.init() #initialize pygame
 
 window = pygame.display.set_mode((640, 480)) #creation et definition des paramètres de la fenetre
 
+player1 = Player()
+
 
 background = pygame.image.load("domo.background.png").convert() #on defini et colle le fond
-window.blit(background, (0, 0))
-
-pygame.display.flip() #rafraichissement de l'image
-
 
 slown = 1
 
 while slown : #boucle qui sert à garder la fenetre ouverte
+
+    window.blit(background, (0, 0))
+    window.blit(player1.image, player1.rect)
+
+    pygame.display.flip() #rafraichissement de l'image
+
     for event in pygame.event.get():
         if event.type == QUIT:
             slown=0
+            pygame.quit()
